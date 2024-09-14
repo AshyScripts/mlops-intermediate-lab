47a213a750a92e263f8d39b23cbe3b2d80878c9d
Ashkan Ghanavati <ashkan.ghanavati92@gmail.com>
2024-09-14 19:50:40 -0400

    added test functions to tests directory

---
+++ tests/test_train_and_save_model.py
@@ -0,0 +1,76 @@
+"""test script for train_and_save_model.py"""
+
+import pytest
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from unittest.mock import patch, MagicMock
+from train_and_save_model import download_data, preprocess_data, train_model, save_model_to_gcs
+
+
+# Test download_data function
+def test_download_data():
+  X, y = download_data()
+  
+  # Check if the data is downloaded correctly
+  assert isinstance(X, pd.DataFrame)
+  assert isinstance(y, pd.Series)
+  assert not X.empty
+  assert not y.empty
+  assert X.shape[0] == y.shape[0]
+  
+# Test preprocess_data function
+def test_preprocess_data():
+  X, y = download_data()
+  X_train, X_test, y_train, y_test = preprocess_data(X, y)
+  
+  assert X_train.shape[0] + X_test.shape[0] == X.shape[0]
+  assert y_train.shape[0] + y_test.shape[0] == y.shape[0]
+  assert X_train.shape[1] == X.shape[1]
+  
+# Test train_model function
+def test_train_model():
+  # Generate sample data
+  X = pd.DataFrame({
+    'sepal length (cm)': [5.1, 4.9, 4.7, 4.6, 5.0],
+    'sepal width (cm)': [3.5, 3.0, 3.2, 3.1, 3.6],
+    'petal length (cm)': [1.4, 1.4, 1.3, 1.5, 1.4],
+    'petal width (cm)': [0.2, 0.2, 0.2, 0.2, 0.2],
+  })
+  
+  y = pd.Series([0, 0, 0, 0, 0])
+  
+  model = train_model(X, y)
+  
+  # Assertions
+  assert isinstance(model, RandomForestClassifier)
+  assert hasattr(model, 'predict')
+  
+# Test save_model_to_gcs function
+# For testing this function, since we are making calls to an external service (GCS), we need to mock GCS objects and client to prevent actual calls.
+def test_save_model_to_gcs():
+  # Create a mock model
+  model = RandomForestClassifier()
+  
+  # Set up a patch context manager for storage.Client
+  with patch('train_and_save_model.storage.Client') as mock_storage_client:
+    mock_bucket = MagicMock()
+    mock_blob = MagicMock()
+    
+    # Define mock bucket and blob objects using chain of return values
+    mock_storage_client.return_value.bucket.return_value = mock_bucket
+    mock_bucket.blob.return_value = mock_blob
+    
+    # Set the exists value on mock blob to return False
+    mock_blob.exists.return_value = False
+    
+    # Call save_model_to_gcs with mock objects and check if the methods were called
+    save_model_to_gcs(model, 'bucket-test', 'blob-test')
+    
+    mock_storage_client.assert_called_once()
+    mock_storage_client.return_value.bucket.assert_called_once_with('bucket-test')
+    
+    assert mock_bucket.blob.call_count == 2
+    mock_bucket.blob.assert_any_call('trained_models/')
+    mock_bucket.blob.assert_any_call('blob-test')
+    mock_blob.upload_from_filename.assert_called_once_with('model.joblib')
+  
